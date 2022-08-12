f75059c596e8eed4f18d1a7d8aaa3d447f711cd8
kevinpho1997 <kevin.pho@griffithuni.edu.au>
2022-08-12 22:42:39 +1000

    moved images to assets folder implemented checking of form inputs against hard coded json values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -18,13 +19,35 @@ export class LoginComponent implements OnInit {
   username: string = "";
   password: string = "";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {this.username = params.get('username')!;} 
+    );
+    this.route.paramMap.subscribe(
+      params => {this.password = params.get('password')!;} 
+    );
   }
 
   loginClicked(){
-    alert("form submitted");
+    // alert("form submitted");
+    var authenticated = false;
+    console.log(this.username);
+    console.log(this.password);
+
+    for(var i=0; i<this.dummyData.users.length; i++){
+      var jsonData = this.dummyData.users;
+      if (jsonData[i]["userName"] == this.username && jsonData[i]["passWord"] == this.password){
+          console.log("Match found");
+          authenticated = true;
+      } 
+    }
+    if (authenticated == true) {
+      this.router.navigateByUrl('/account');
+    } else if (authenticated == false) {
+      alert("Incorrect username or password");
+    }
   }
 }
 
