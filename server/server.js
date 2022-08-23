ad5c645659a159048ae5dcbc92aa4b303fb20632
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-08-24 00:02:16 +1000

    added required empty files

--- server/server.js
+++ server/server.js
@@ -1,35 +1,45 @@
-var express = require("express");
-var app = express();
-var http = require('http').Server(app);
-app.use(express.static(__dirname + '/www'));
+// week 5.3 lecture notes
+const express = require("express");
+const app = express();
+
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../dist/week4tut')));
+
+const path = require('path');
+const http = require('http').Server(app);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 import {User} from "./routes/users.ts";
 
 let server = http.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log("My first Nodejs server!");
-    console.log(`Server listening on: ${host}, port: ${port}`);
+    // let host = server.address().address;
+    // let port = server.address().port;
+    console.log(`Server listening on port 3000`);
 });
 
-app.get('/', function(req, res) {
-    res.send("Hello World");
-}) 
+app.post('/login', require('./routes/postLogin'));
+app.post('/loginafter', require('./routes/postLoginAfter')); 
 
 // app.get('api/auth', function(req, res) {
 //     console.log("@ Get Auth");
 //     res.sendFile(__dirname + '/www/api/auth.html');
 // });
 
-app.post('api/auth', function(req, res) {
-    console.log("@ Post Auth");
+// app.post('api/auth', function(req, res) {
+//     console.log("@ Post Auth");
 
-    let user1 = new User("kevinpho", "18/07/1997", 25, "kevin.pho@", "yep", false);
-    let user2 = new User("yeji", "01/01/2000", 22, "yeji@email.com", "nope", false);
-    let user3 = new User("dahyun", "01/01/1998", 24, "dahyun@email.com", "maybe", false);
+//     let user1 = new User("kevinpho", "18/07/1997", 25, "kevin.pho@", "yep", false);
+//     let user2 = new User("yeji", "01/01/2000", 22, "yeji@email.com", "nope", false);
+//     let user3 = new User("dahyun", "01/01/1998", 24, "dahyun@email.com", "maybe", false);
 
-    let users = [user1, user2, user3];
+//     let users = [user1, user2, user3];
 
-    // for (let i = 0; i < users.length; i++){
+//     // for (let i = 0; i < users.length; i++){
         
-    // }
-});
+//     // }
+// });
