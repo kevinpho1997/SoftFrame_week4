bf5e0b199888facf8ed2fed71aad977ae9724780
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-08-26 04:31:54 +1000

    added temp form added temp vars for getting sessionstorage

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  username = sessionStorage.getItem('username');
+  // not sure if this should be here
+  password = sessionStorage.getItem('password');
+  birthdate = sessionStorage.getItem('birthdate');
+  age = sessionStorage.getItem('age');
+  email = sessionStorage.getItem('email');
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginfunc(){
+
   }
 
 }
